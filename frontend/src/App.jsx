a470008fdc96be710f2c33f4384dbcf551cbb159
victor <victor.verma@icloud.com>
2023-11-27 22:40:19 -0500

    set up notistack

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
 import Home from "./pages/Home/Home";
 import Academics from "./pages/Academics/Academics";
@@ -11,13 +12,15 @@ import "./App.css";
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/academics" element={<Academics />} />
-        <Route path="/professional" element={<Professional />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <SnackbarProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/academics" element={<Academics />} />
+          <Route path="/professional" element={<Professional />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </SnackbarProvider>
     </div>
   );
 };
