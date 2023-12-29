4597ff268d1dc8c8fcde51ed36cc7c66fa17a068
victor <victor.verma@icloud.com>
2023-12-29 10:56:42 -0500

    removed unnecessary import

--- frontend/src/pages/Academics/CourseList.jsx
+++ frontend/src/pages/Academics/CourseList.jsx
@@ -1,4 +1,4 @@
-import { useParams, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Breadcrumb } from "react-bootstrap";
