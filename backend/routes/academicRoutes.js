31c5b806594b19cb8dc626951b6c37d2522d6e4b
victor <victor.verma@icloud.com>
2024-01-05 00:06:09 -0500

    imported math nodes and edges

--- backend/routes/academicRoutes.js
+++ backend/routes/academicRoutes.js
@@ -1,20 +1,21 @@
 import express from "express";
 
 // course information imports
-import bmeCourses from "./../data/courses/course-info/undergrad/biomedical-eng-ug-course-info.json" assert { type: "json" };
-import csCourses from "./../data/courses/course-info/undergrad/computer-science-ug-course-info.json" assert { type: "json" };
-import dsCourses from "./../data/courses/course-info/undergrad/data-science-ug-course-info.json" assert { type: "json" };
-import econCourses from "./../data/courses/course-info/undergrad/economics-ug-course-info.json" assert { type: "json" };
-import eceCourses from "./../data/courses/course-info/undergrad/electrical-computer-eng-ug-course-info.json" assert { type: "json" };
-import engCoreCourses from "./../data/courses/course-info/undergrad/eng-core-ug-course-info.json" assert { type: "json" };
-import mathCourses from "./../data/courses/course-info/undergrad/mathematics-statistics-ug-course-info.json" assert { type: "json" };
-import mecheCourses from "./../data/courses/course-info/undergrad/mechanical-eng-ug-course-info.json" assert { type: "json" };
+import bmeCourses from "../data/courses/course-info/undergrad/biomedical-eng-ug-course-info.json" assert { type: "json" };
+import csCourses from "../data/courses/course-info/undergrad/computer-science-ug-course-info.json" assert { type: "json" };
+import dsCourses from "../data/courses/course-info/undergrad/data-science-ug-course-info.json" assert { type: "json" };
+import econCourses from "../data/courses/course-info/undergrad/economics-ug-course-info.json" assert { type: "json" };
+import eceCourses from "../data/courses/course-info/undergrad/electrical-computer-eng-ug-course-info.json" assert { type: "json" };
+import engCoreCourses from "../data/courses/course-info/undergrad/eng-core-ug-course-info.json" assert { type: "json" };
+import mathCourses from "../data/courses/course-info/undergrad/mathematics-statistics-ug-course-info.json" assert { type: "json" };
+import mecheCourses from "../data/courses/course-info/undergrad/mechanical-eng-ug-course-info.json" assert { type: "json" };
 
 // course dependency map imports
 import csEdges from "./../data/courses/dependencies/edges/computer-science-dependency-edges.json" assert { type: "json" };
 import csNodes from "./../data/courses/dependencies/nodes/computer-science-dependency-nodes.json" assert { type: "json" };
 import dsEdges from "../data/courses/dependencies/edges/data-science-dependency-edges.json" assert { type: "json" };
 import dsNodes from "../data/courses/dependencies/nodes/data-science-dependency-nodes.json" assert { type: "json" };
+import mathEdges from "../data/courses/dependencies/edges/mathematics-statistics-dependency-edges.json" assert { type: "json" };
 import mathNodes from "../data/courses/dependencies/nodes/mathematics-statistics-dependency-nodes.json" assert { type: "json" };
 
 const subjectMap = {
@@ -38,7 +39,7 @@ const subjectMap = {
     "eng-core": { "course-info": engCoreCourses, edges: "", nodes: "" },
     "mathematics-statistics": {
         "course-info": mathCourses,
-        edges: "",
+        edges: mathEdges,
         nodes: mathNodes,
     },
     "mechanical-eng": { "course-info": mecheCourses, edges: "", nodes: "" },
