de07bdccbe799f3eba6dba8ea8c359fef7bbe4bb
victor <victor.verma@icloud.com>
2024-01-05 00:12:05 -0500

    deleted files

--- backend/routes/calendarRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-const router = express.Router()
-
-router.get("/", (res, req) => {
-    console.log(req);
-    res.status(234).send("Calendar backend here");
-});
-
-export default router
