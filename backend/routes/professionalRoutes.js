42f2ebe0aa1bc217045d5b7a7630a96053e8a033
victor <victor.verma@icloud.com>
2024-01-05 00:07:25 -0500

    renamed files -> added an s

---
+++ backend/routes/professionalRoutes.js
@@ -0,0 +1,14 @@
+import express from "express"
+const router = express.Router()
+
+router.get("/", (res, req) => {
+    console.log(req);
+    res.status(234).send("Professional backend here");
+});
+
+router.get("/resources", (res, req) => {
+    console.log(req);
+    res.status(234).send("Professional resources backend here");
+});
+
+export default router
