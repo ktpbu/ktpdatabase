5ceb2b99397ee44fcb5a40f7bc0e35b533b65a77
victor <vpverm@bu.edu>
2024-06-14 23:52:41 -0400

    improved add review form

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
+            border: {
+                1: "1px",
+            },
             height: {
                 128: "512px",
             },
