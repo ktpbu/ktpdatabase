85ffe4f0a1d6c2041d3095fba080e9ae363ad612
victor <vpverm@bu.edu>
2024-06-15 19:56:04 -0400

    migrated header to tailwind css

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
         extend: {
             border: {
                 1: "1px",
@@ -11,5 +18,6 @@ export default {
             },
         },
     },
+
     plugins: [],
 };
