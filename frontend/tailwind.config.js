371e677d2ec90479410eb767f69f2b45b40ad862
victor <vpverm@bu.edu>
2024-06-14 21:32:59 -0400

    migrated more pages to tailwindcss and removed need for page-content css file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            height: {
+                128: "512px",
+            },
+        },
     },
     plugins: [],
 };
