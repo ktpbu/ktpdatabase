d7bff6bc2eaa84370bae61b51b79ba62323da421
Nicholas David <nicholasdavid@Nicks-M1-MacBook.local>
2024-06-18 22:35:59 -0700

    added working links to footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -9,9 +9,15 @@ const Footer = () => {
                 &#169; 2023, Kappa Theta Pi Lambda Chapter
             </div>
             <div>
-                <InstagramIcon className="mx-2" />
-                <LinkedInIcon className="mx-2" />
-                <GitHubIcon className="mx-2" />
+            <a href="https://www.instagram.com/ktpbostonu/" target="_blank" rel="noopener noreferrer"style={{ color: 'inherit', textDecoration: 'none' }}>
+                    <InstagramIcon className="mx-2" />
+                </a>
+                <a href="https://www.linkedin.com/company/kappa-theta-pi-lambda-chapter/" target="_blank" rel="noopener noreferrer"style={{ color: 'inherit', textDecoration: 'none' }}>
+                    <LinkedInIcon className="mx-2" />
+                </a>
+                <a href="https://github.com/ktpbu" target="_blank" rel="noopener noreferrer"style={{ color: 'inherit', textDecoration: 'none' }}>
+                    <GitHubIcon className="mx-2" />
+                </a>
             </div>
         </footer>
     );
