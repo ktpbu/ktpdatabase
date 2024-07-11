a82c861444476cb346d07fe6d4432365ed9363ae
victor <vpverm@bu.edu>
2024-07-11 15:35:31 -0400

    added footer link to ktp website

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 import GitHubIcon from "@mui/icons-material/GitHub";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import LanguageIcon from "@mui/icons-material/Language";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
@@ -13,17 +14,30 @@ const Footer = () => {
             <div className="w-fit my-3">
                 <Link
                     className="mx-2 text-white"
+                    to="https://ktpbostonu.com/"
+                    target="_blank"
+                >
+                    <LanguageIcon />
+                </Link>
+                <Link
+                    className="mx-2 text-white"
                     to="https://www.instagram.com/ktpbostonu/"
+                    target="_blank"
                 >
                     <InstagramIcon />
                 </Link>
                 <Link
                     className="mx-2 text-white"
                     to="https://www.linkedin.com/company/kappa-theta-pi-lambda-chapter/"
+                    target="_blank"
                 >
                     <LinkedInIcon />
                 </Link>
-                <Link className="mx-2 text-white" to="https://github.com/ktpbu">
+                <Link
+                    className="mx-2 text-white"
+                    to="https://github.com/ktpbu"
+                    target="_blank"
+                >
                     <GitHubIcon />
                 </Link>
             </div>
