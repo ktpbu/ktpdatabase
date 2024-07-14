535bdf5b7e5320c9fb7cce18882749deb111ea53
victor <vpverm@bu.edu>
2024-07-14 17:55:41 -0400

    removed top-level await

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -19,6 +19,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-await setPersistence(auth, browserSessionPersistence);
+setPersistence(auth, browserSessionPersistence);
 
 export const googleProvider = new GoogleAuthProvider();
