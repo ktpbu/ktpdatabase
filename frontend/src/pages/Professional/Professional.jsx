5c1244eaedcaa31b3ee01278d6fb6fb31a618db6
victor <vpverm@bu.edu>
2024-07-17 22:21:26 -0400

    standardized card icons

--- frontend/src/pages/Professional/Professional.jsx
+++ frontend/src/pages/Professional/Professional.jsx
@@ -1,21 +1,20 @@
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import GroupIcon from "@mui/icons-material/Group";
+import WorkIcon from "@mui/icons-material/Work";
 
 import CustomBreadcrumb from "../../components/CustomBreadcrumb";
-
-import ktplogolandscape from "./../../assets/ktplogolandscape.png";
-import internship from "./../../assets/intern.png";
 
 const professionalOptions = [
     {
         link: "/professional/other-ktp-chapters",
-        image: ktplogolandscape,
+        image: <GroupIcon className="my-auto" />,
         header: "KTP Chapters",
         text: "Links to websites of other KTP chapters",
     },
     {
         link: "/professional/Internships",
-        image: internship,
+        image: <WorkIcon className="my-auto" />,
         header: "Internships and New Grad",
         text: "Useful resources for getting internships",
     },
@@ -40,12 +39,12 @@ const Professional = () => {
                     >
                         <Card className="w-72 sm:w-96 flex flex-col text-start text-lg duration-200 hover:bg-[#baecba] hover:scale-105">
                             <Card.Header>
-                                <img
-                                    className="w-auto h-8 mr-2.5"
-                                    src={option.image}
-                                    alt={option.header}
-                                />
-                                <b>{option.header}</b>
+                                <div className="flex">
+                                    {option.image}
+                                    <b className="ml-3 my-auto">
+                                        {option.header}
+                                    </b>
+                                </div>
                             </Card.Header>
                             <Card.Body className="h-32">
                                 <Card.Text className="text-sm text-gray-500">
